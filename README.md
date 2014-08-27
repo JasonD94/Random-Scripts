6906f3946467383f75e8d3492debb99384ed8463
Jason <jason_downing@student.uml.edu>
2014-08-26 21:53:54 -0400

    working backup script, yay

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Couple of notes:
 speaker beep. It might also include other random stuff found on the ubuntu repos.
 - I run Xubuntu 14.04 LTS, so these should work on other Ubuntu/Debian based distros.
 
-1. rsync backup - this one will backup my LG G3. It even beeps to let me know its done! :o
+1. rsync backup - this one will backup my LG G3. It even beeps to let me know its done! :o (Version 1.3 is working! I wanted to autorun the script whenever I plug my G3 in to charge, but MTP wouldn't automount. The most I can do is make a script to yell at me to mount the G3 and run the backup script. :|)
 2. beep music - I found a bunch of these and wanted to save them for use later.
 3. install related scripts - mass installs ~30 programs I use.
 4. A sync to fix my mouse's DPI. Two line script but it should make life easier.
---
+++ rsync_scripts/backup_android_v1.2.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#   V1.2
+#   NOTES
+#   Requires lsusb, rsync, beep
+#   & and a LG G3 (other phones, try replacing "LG" with your brand)
+#   Also - credit for some of this comes from:
+#   http://askubuntu.com/questions/342319/where-are-mtp-mounted-devices-located-in-the-filesystem
+
+#*******************************************************************************************
+# 	I couldn't figure out how to auto launch this script on device plug in. >_<
+# 	What I suggest is to just create a launcher on your desktop called "Backup Android"
+# 	and run it whenever you need to charge your phone. Just plug it into your PC, make sure
+# 	the device is mounted (check your file manager and click on the device to make sure you
+#	see "Internal" and "SD_Card" listed) and click on the launcher, which should be setup to
+# 	find this script and run it in a terminal (so you can see if it worked or not)
+#*******************************************************************************************
+
+####################################################################################
+# IGNORE THIS IT DOESN'T SEEM TO WORK FOR ME. MAYBE SOMEONE CAN USE THIS THOUGH. :\
+# So to auto run this upon USB being plugged in, I suggest looking at the
+# ".rules" file in this directory. If placed in:
+# /etc/udev/rules.d/FILE_GOES_HERE
+# Then the script should auto run. See the below thread for more information:
+# http://stackoverflow.com/questions/4189383/bash-script-to-detect-when-my-usb-is-plugged-in-and-to-then-sync-it-with-a-direc
+# Actually, try this one:
+# http://unix.stackexchange.com/questions/65891/how-to-execute-a-shellscript-when-i-plug-in-a-usb-device
+# Since it actually made beeping noises.
+# Next, we redirect all these echos to a log file for debugging.
+# Also, we'll need to sleep the command for 20 seconds to let the G3 mount properly.
+# I was going to auto mount my 1TB drive, but I set it up to automount on boot using:
+# https://help.ubuntu.com/community/MountingWindowsPartitions
+# Also I set up my G3 to automount using the below page:
+# https://wiki.archlinux.org/index.php/MTP
+# (I use Xubuntu 14.04 LTS, so the "gvfs-mtp" part was most useful!)
+################################################################################
+
+# Variables set here. Change any paths, device brands, etc here.
+DEVICE_BRAND='LG'
+
+# These are locations on your computer! Ex: your 1TB hard drive.
+# You could also use your home directory if you have enough room.
+INTERNAL_BACKUP_PATH='/media/Data/_G3_Backup/Internal' 
+EXTERNAL_BACKUP_PATH='/media/Data/_G3_Backup/SD_Card'
+#################################################################################
+
+# Not necessary, but I like to grab the start time.
+start=$(date +%s)
+
+# Add some space and the date to the log file.
+printf "\n\nANDROID AUTOBACKUP."
+cur_date=$(date)
+echo " Backup Date: $cur_date"
+printf "\n"
+
+# Need to get the PC speaker working for the BEEPs at the end.
+# Comment this & the beep command out if you don't like a beeping computer!
+sudo modprobe pcspkr
+
+# Detect changes to the MTP protcol
+# Also - this was made for an LG G3. If you own a different device,
+# change the last part of the "grep" command. See what the output of
+# lsusb gives you for a manufacturer and replace the "LG" with that. 
+echo "FULL DEVICE INFO: "
+OUTPUT="$(lsusb | grep LG)"
+printf "*****************************************************\n"
+echo $OUTPUT
+printf "*****************************************************\n"
+
+# Get the USB_BUS # of our phone
+usb_bus="${OUTPUT% Device*}"
+usb_bus="${usb_bus#Bus }"
+echo "USB BUS #: " $usb_bus # DEBUGGING
+
+# Now let's get the USB_DEVICE # of the phone
+usb_device="${OUTPUT%%:*}"
+usb_device="${usb_device#*Device }"
+echo "USB DEVICE #: " $usb_device
+
+# Now we can get the path for our sync! YAY!
+MTP_PATH="/run/user/1000/gvfs/mtp:host=%5Busb%3A${usb_bus}%2C${usb_device}%5D"
+echo "MTP PATH: " $MTP_PATH
+
+# This is the first sync - Internal Memory.
+printf "\n\nSTART OF INTERNAL SYNC\n"
+
+# rsync internal phone memory
+# Note: >> and 2>&1 redirects the output to a log file in /tmp/
+rsync -avzh --progress --exclude-from 'exclude_internal.txt' \
+$MTP_PATH/Internal\ storage/ \
+$INTERNAL_BACKUP_PATH
+
+# This is the second sync - External SD card.
+printf "\n\nSTART OF EXTERNAL SD CARD SYNC\n"
+
+# rsync external SD card
+rsync -avzh --progress --exclude-from 'exclude_sdcard.txt' \
+$MTP_PATH/SD\ card/ \
+$EXTERNAL_BACKUP_PATH
+
+# Let's get the total time it took!
+end=$(date +%s)
+total=$((end-start))
+
+# Print we done yo to ze screen.
+printf "***********************************************************************************\n"
+echo   "	FINISHED SYNC. TOTAL TIME THIS TOOK: " $total "seconds"
+printf "***********************************************************************************\n"
+
+# Beep when it is done.
+beep -r 5
